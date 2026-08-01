339113e450143ee603930357f6e64c364a5a9bf7
Henri <henrichoule@gmail.com>
2026-08-01 18:14:10 -0400

    Added boilerplate Flask logic and fixed venv problems

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
 
-# Configure applications
+# Configure application
 app = Flask(__name__)
 
-@app.route("/")
+# Reload templates when they are changed
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route("/", methods=["GET", "POST"])
 def hello_world():
-    return render_template("index.html")
+    if request.method == "POST":
+
+#TODO:  Write Python code to take a claim string, search for it via DuckDuckGo/Google Books/Podcast Index/YouTube, 
+        # fetch results, and collect titles and links. Print out a rough list of findings
+        
+        belief = request.form.get("belief")
+
+        if not belief:
+            return redirect("/")
+# TODO: Redirect to different html page that has the results.
+        return redirect("/") 
+
+    else:
+        return render_template("index.html")
 
 
